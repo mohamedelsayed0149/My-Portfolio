ba0aab0a9d6ecd280b3c3af5bf59918f5ecd7891
mohamedelsayed0149 <mohamed11490@gmail.com>
2021-01-18 04:11:47 +0200

    My Portfolio

---
+++ src/components/Banner.js
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaPinterest,
+  FaInstagram,
+  FaPlay,
+} from "react-icons/fa";
+const Banner = () => {
+  const [state] = React.useState({
+    title: "I am  Mohamed El Sayed",
+    text:
+      "i,m Mohamed, professional Frontend web developer with long time experience in this field​",
+  });
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <div className="header__content">
+              <div className="header__section">
+                <ul className="header__ul">
+                  <li>
+                    <FaFacebookF className="headerIcon" />
+                  </li>
+                  <li>
+                    <FaTwitter className="headerIcon" />
+                  </li>
+                  <li>
+                    <FaPinterest className="headerIcon" />
+                  </li>
+                  <li>
+                    <FaInstagram className="headerIcon" />
+                  </li>
+                </ul>
+                <h1>{state.title}</h1>
+                <p>{state.text}</p>
+                <div className="header__buttons">
+                  <a href=" " className="btn btn-outline">
+                    My Portfolio
+                  </a>
+                  &nbsp;&nbsp;&nbsp;
+                  <a href=" " className="btn btn-smart">
+                    <FaPlay className="play" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Banner;
